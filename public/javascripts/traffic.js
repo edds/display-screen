27c01f710fb9887ec9337ecc4d389879adf3c0a1
Edd Sowden <e@e26.co.uk>
2014-10-28 11:08:28 +0000

    Better date logic for traffic data

--- public/javascripts/traffic.js
+++ public/javascripts/traffic.js
@@ -6,9 +6,10 @@
 
   var traffic = {
     $el: false,
+    startDate: false,
 
     endpoint: function(offset){
-      var date = new Date(),
+      var date = new Date(traffic.startDate),
           start, end;
 
       date.setDate(date.getDate() - offset);
@@ -19,51 +20,77 @@
       return "https://www.performance.service.gov.uk/data/government/realtime?sort_by=_timestamp%3Adescending"
             +"&start_at="+ start +"&end_at="+ end;
     },
+    pointsToArray: function(data){
+      var i, _i,
+          date = new Date(traffic.startDate),
+          dates = [],
+          dataByTime = {},
+          key,
+          values = [];
+
+      // find all the minutes we need
+      for(i=0,_i=60*24; i<_i; i++){
+        date.setMinutes(date.getMinutes() - 1);
+        dates.push(date.getHours() +":"+ date.getMinutes());
+      }
+
+      // turn the data points into a time key object
+      for(i=0, _i=data.length; i<_i; i++){
+        date = new Date(data[i]._timestamp);
+        key = date.getHours() +":"+ date.getMinutes();
+        dataByTime[key] = parseInt(data[i].unique_visitors, 10);
+      }
+
+      // create an array of the data in the right order
+      for(i=0,_i=dates.length; i<_i; i++){
+        if(dataByTime[dates[i]]){
+          values.push(dataByTime[dates[i]]);
+        } else {
+          // fill missing minutes, don't fill more than two though
+          if(values.length && values[values.length-1] !== values[values.length-2]){
+            values.push(values[values.length-1]);
+          } else {
+            values.push(0);
+          }
+        }
+      }
+      return values.reverse();
+    },
     parseTrendline: function(data){
       var i, _i,
           j, _j,
-          newSeries, newData = [],
-          dates = {},
-          key, date,
           total,
           values,
           trend = [];
 
       for(j=0, _j=data.length; j<_j; j++){
-        newSeries = {};
-        for(i=0, _i=data[j].data.length; i<_i; i++){
-          date = new Date(data[j].data[i]._timestamp);
-          key = date.getHours() +":"+ date.getMinutes();
-          newSeries[key] = parseInt(data[j].data[i].unique_visitors, 10);
-          dates[key] = 1;
-        }
-        newData.push(newSeries);
+        data[j] = traffic.pointsToArray(data[j]);
       }
 
-      dates = Object.keys(dates).reverse();
-
-      for(i=0, _i=dates.length; i<_i; i++){
+      for(i=0, _i=data[0].length; i<_i; i++){
         total = 0,
         values = 0;
-        for(j=0, _j=newData.length; j<_j; j++){
-          if(newData[j][dates[i]]){
+        for(j=0, _j=data.length; j<_j; j++){
+          if(data[j][i]){
             values = values + 1;
-            total = total + newData[j][dates[i]];
+            total = total + data[j][i];
           }
         }
-        trend.push( total / values );
+        if(values === 0){
+          trend.push(0);
+        } else {
+          trend.push( total / values );
+        }
       }
       return trend;
     },
-    parseResponse: function(data, past1, past2, past3, past4){
+    parseResponse: function(data, past1, past2, past3){
       var counts = [],
           i, _i,
-          trend = traffic.parseTrendline([past1[0], past2[0], past3[0], past4[0]]);
+          trend = traffic.parseTrendline([past1[0].data, past2[0].data, past3[0].data]);
 
       traffic.$el.html('<h1>' + root.matrix.numberWithCommas(data[0].data[0].unique_visitors) + '</h1>');
-      for(i=0,_i=data[0].data.length; i<_i; i++){
-        counts.unshift(parseInt(data[0].data[i].unique_visitors, 10));
-      }
+      counts = traffic.pointsToArray(data[0].data);
       if(typeof traffic.sparkline === 'undefined'){
         traffic.sparkline = root.matrix.sparklineGraph('#traffic-count-graph',
               { data: counts,
@@ -83,12 +110,12 @@
       window.setInterval(traffic.reload, 20e3);
     },
     reload: function(){
+      traffic.startDate = new Date();
       $.when(
         $.ajax({ dataType: 'json', url: traffic.endpoint(0) }),
         $.ajax({ dataType: 'json', url: traffic.endpoint(7) }),
         $.ajax({ dataType: 'json', url: traffic.endpoint(14) }),
-        $.ajax( { dataType: 'json', url: traffic.endpoint(21) }),
-        $.ajax( { dataType: 'json', url: traffic.endpoint(28) })
+        $.ajax( { dataType: 'json', url: traffic.endpoint(21) })
       ).then(traffic.parseResponse);
     }
   };
